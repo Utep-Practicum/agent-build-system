349f67bcbcb1357ccfdde57a34edf4fb69f53e6c
dlramirez2 <dlramirez2@miners.utep.edu>
2021-05-04 23:03:27 -0600

    Click Update

--- Builder/script_generator.py
+++ Builder/script_generator.py
@@ -51,7 +51,7 @@ class ScriptGenerator:
 
     def translate_observation_to_script_command(self, observation):
         if observation.data_type == "imgPoint" and observation.is_click:
-            print(f"pyautogui.onScreen(x={observation.coordinateX}, y={observation.coordinateY})")
+            print(f"pyautogui.click(x={observation.coordinateX}, y={observation.coordinateY})")
         elif observation.data_type == "Keypresses":
             string_to_print = ""
             for char in observation.data["content"]:
